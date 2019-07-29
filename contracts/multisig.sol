0d2f7271d6dc7784305ff25fe390520ed19665e2
Sumitjh26997 <sumit.hotchandani@gmail.com>
2019-07-30 02:05:14 +0530

    Add: Some modifiers, data retrieval functions and housekeeping

--- contracts/multisig.sol
+++ contracts/multisig.sol
@@ -1,6 +1,8 @@
-pragma solidity v0.4.24;
+pragma solidity 0.4.24;
 
 contract multisig{
+
+    uint MAX_OWNERS = 50;
 
     struct Transaction{
         address destination;
@@ -9,19 +11,69 @@ contract multisig{
         bool executed;
     }
 
+    /* 
+    ---------------------------
+    Mappings
+    --------------------------- 
+    */
+
     mapping (uint => Transaction) public transactions;
     mapping (uint => mapping (address => bool)) public confirmations;
     mapping (address => bool) public isOwner;
+
+    /* 
+    ---------------------------
+    Global variables
+    --------------------------- 
+    */
+
     uint public required;
     uint public transactionCount;
     address[] public owners;
 
-    event addOwner(address newOwner);
-    event removeOwner(address oldOwner);
-    event requirementChanged(uint required);
+    /* 
+    ---------------------------
+    Events
+    --------------------------- 
+    */
 
+    event addOwner(address newOwner);
+    event removedOwner(address oldOwner);
+    event requirementChanged(uint required);
+    event transactionSubmit(uint transactionId);
+    event Deposit(address sender, uint value);
+
+    /* 
+    ---------------------------
+    Modifiers
+    --------------------------- 
+    */
+ 
+    modifier isNotAnOwner(address _owner){
+        require(isOwner[_owner] == false, "Owner already exists");
+        _;
+    }
+
+    modifier isAnOwner(address _owner){
+        require(isOwner[_owner] == true, "Owner does not exist");
+        _;
+    }
+
+    modifier notNull(address _address) {
+        require(_address == 0, "Null Address");
+        _;
+    }
+
+
+    // fallback function to deposit ethers
+    function () payable public {
+        if(msg.value > 0)
+            emit Deposit(msg.sender,msg.value);
+    }
+
+    
     //check max length, check if address is valid
-    function multisig(address[] _owners, uint _required) public{
+    constructor(address[] _owners, uint _required) public{
         for(uint i = 0; i<_owners.length; i++)
             isOwner[_owners[i]] = true;
 
@@ -30,10 +82,10 @@ contract multisig{
     }
 
     //check max length, check if owner does not already exists, check if address is valid
-    function addNewOwner(address _owner) public{
+    function addNewOwner(address _owner) isNotAnOwner(_owner) public{
         isOwner[_owner] = true;
         owners.push(_owner);
-        addOwner(_owner);
+        emit addOwner(_owner);
     }
 
     //check if owner exists
@@ -45,10 +97,10 @@ contract multisig{
                 break;
             }
         }
-        onwers.length -= 1;
+        owners.length -= 1;
         if(required > owners.length)
             changeRequired(owners.length);
-        removeOwner(_owner);
+        emit removedOwner(_owner);
     }
 
 
@@ -62,15 +114,73 @@ contract multisig{
 
             isOwner[_owner] = false;
             isOwner[_newOwner] = true;
-            removeOwner(_owner);
-            addOwner(_newOwner);
+            emit removedOwner(_owner);
+            emit addOwner(_newOwner);
         }
     }
 
     //check max length
     function changeRequired(uint _required) public{
         required = _required;
-        requirementChanged(_required);
+        emit requirementChanged(_required);
     }
 
+    function addTransaction(address _destination, uint _value, bytes _data) internal returns(uint){
+        transactionId = transactionCount;
+        transactions[transactionId] = Transaction({
+            destination: _destination,
+            value: _value,
+            data: _data,
+            executed: false
+        });
+        transactionCount += 1;
+        emit transactionSubmit(transactionId);
+        return transactionId;
+    }
+
+    // function submitTransaction(address _destination, uint _value, bytes _data) public{
+    // }
+
+
+
+    /* 
+    ---------------------------
+    Data retrieval functions
+    --------------------------- 
+    */
+
+    // get no. of confirmations for a transaction
+    function getConfirmations(uint transactionId) public external view returns(uint){
+        uint confirmationCount = 0
+        
+        for(uint i = 0; i < owners.length; i++)
+        {
+            if(confirmations[transactionId][owners[i]])
+                confirmationCount += 1;
+        }
+
+        return confirmationCount;   
+    }
+
+    // return list of addresses of owners of the wallet
+    function getOwners() public external view returns(address[]){
+        return owners;
+    }
+
+    // return list of addresses of owners who have confirmed a transaction
+    function getOwnerConfirmations(uint transactionId) public external view returns (address[])
+    {
+        address[] memory ownerConfirmationsTemp = new address[](owners.length);
+        uint count = 0;
+        for (uint i=0; i < owners.length; i++)
+            if (confirmations[transactionId][owners[i]]) {
+                ownerConfirmationsTemp[count] = owners[i];
+                count += 1;
+            }
+        ownerConfirmations = new address[](count);
+        for (uint i=0; i < count; i++)
+            ownerConfirmations[i] = ownerConfirmationsTemp[i];
+
+        return ownerConfirmations;
+    }
 }
